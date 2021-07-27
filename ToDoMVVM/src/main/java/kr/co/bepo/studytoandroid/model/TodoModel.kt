c3ce9e1611c4cdfe1f777d39f09903a63fb84540
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-07-27 22:36:57 +0900

    MVVM 패턴 사용, 레이아웃 구성

---
+++ ToDoMVVM/src/main/java/kr/co/bepo/studytoandroid/model/TodoModel.kt
@@ -0,0 +1,7 @@
+package kr.co.bepo.studytoandroid.model
+
+data class TodoModel(
+    var title: String,
+    var description: String,
+    var createdDate: Long
+)
---
+++ ToDoMVVM/src/main/java/kr/co/bepo/studytoandroid/view/TodoListActivity.kt
@@ -0,0 +1,10 @@
+package kr.co.bepo.studytoandroid.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TodoListActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
