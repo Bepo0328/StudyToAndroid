e34212b4990131b7bbc4f3543843e06ba0cbfa6d
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-07-29 03:43:57 +0900

    MVVMTodo:  스와이프로 삭제, 되돌리기 기능 구현

--- mvvmtodo/src/main/java/kr/co/bepo/mvvmtodo/ui/tasks/TasksViewModel.kt
+++ mvvmtodo/src/main/java/kr/co/bepo/mvvmtodo/ui/tasks/TasksViewModel.kt
@@ -4,9 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import kr.co.bepo.mvvmtodo.data.PreferencesManager
 import kr.co.bepo.mvvmtodo.data.SortOrder
@@ -21,8 +23,10 @@ class TasksViewModel @Inject constructor(
 ) : ViewModel() {
 
     val searchQuery = MutableStateFlow("")
-
     val preferencesFlow = preferencesManager.preferencesFlow
+
+    private val taskEventChannel = Channel<TaskEvent>()
+    val tasksEvent = taskEventChannel.receiveAsFlow()
 
     private val tasksFlow = combine(
         searchQuery,
@@ -32,6 +36,8 @@ class TasksViewModel @Inject constructor(
     }.flatMapLatest { (query, filterPreferences) ->
         taskDao.getTasks(query, filterPreferences.sortOrder, filterPreferences.hideCompleted)
     }
+
+    val tasks = tasksFlow.asLiveData()
 
     fun onSortOrderSelected(sortOrder: SortOrder) = viewModelScope.launch {
         preferencesManager.updateSortOrder(sortOrder)
@@ -48,5 +54,16 @@ class TasksViewModel @Inject constructor(
         taskDao.update(task.copy(completed = isChecked))
     }
 
-    val tasks = tasksFlow.asLiveData()
+    fun onTaskSwiped(task: Task) = viewModelScope.launch {
+        taskDao.delete(task)
+        taskEventChannel.send(TaskEvent.ShowUndoDeleteTaskMessage(task))
+    }
+
+    fun onUndoDeleteClick(task: Task) = viewModelScope.launch {
+        taskDao.insert(task)
+    }
+
+    sealed class TaskEvent {
+        data class ShowUndoDeleteTaskMessage(val task: Task) : TaskEvent()
+    }
 }
